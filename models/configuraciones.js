26ab5efb0a702cc2a1da35babe6875c7764f1ff8
wfpc92 <wfpc92@hotmail.com>
2016-08-25 13:17:45 -0500

    implementacion de configuraciones

--- models/configuraciones.js
+++ models/configuraciones.js
@@ -1,8 +1,20 @@
 var mongoose = require('mongoose');
 
 var ConfiguracionesSchema   = new mongoose.Schema({
+	versionInventario: {
+		type: Number,
+		required: true
+	},
 	domicilio: {
 		type: Number,
+		required: true
+	}, 
+	sobreEmpresa: {
+		type: String,
+		required: true
+	}, 
+	terminosCondiciones: {
+		type: String,
 		required: true
 	}
 },{
--- routes/configuraciones.js
+++ routes/configuraciones.js
@@ -3,6 +3,15 @@ var router = express.Router();
 
 var Configuraciones = require('../models/configuraciones');
 
+function crearConfiguraciones(config) {
+	var configuraciones = new Configuraciones({
+		domicilio: config.domicilio || 0,
+		versionInventario: config.versionInventario || 0,
+		sobreEmpresa: config.sobreEmpresa || "Información sobre ésta empresa no editada",
+		terminosCondiciones: config.terminosCondiciones || "Terminos y condiciones no editadas"
+	});
+	return configuraciones;
+}
 
 module.exports = function(app, passport) {
 	router.use(passport.authenticate('jwt', { session: false}));
@@ -11,9 +20,8 @@ module.exports = function(app, passport) {
 		Configuraciones.find(function(err, configuracionesLst) {
 			if (err) return res.json({success: false, mensaje: err.errmsg, error: err});
 
-			var configuraciones = {
-				domicilio: 0
-			};
+			var configuraciones = crearConfiguraciones({});
+
 			if(configuracionesLst.length !== 0){
 				configuraciones = configuracionesLst[0];
 			}
@@ -26,8 +34,52 @@ module.exports = function(app, passport) {
 		});
 	});
 
+	router.get('/version-inventario', function(req, res) {
+		Configuraciones.find(function(err, configuracionesLst) {
+			if (err) return res.json({success: false, mensaje: err.errmsg, error: err});
+
+			var versionInventario = 0;
+
+			if(configuracionesLst.length !== 0){
+				versionInventario = configuracionesLst[0].versionInventario;
+			}
+
+			res.json({
+				success: true,
+				versionInventario: versionInventario,
+				mensaje: 'version de inventario actual del API.'
+			});
+		});
+	});
+
 
 	router.post('/domicilio',	function(req, res){
+		Configuraciones.find(function(err, configuracionesLst) {
+			if (err) return res.json({success: false, mensaje: err.errmsg, error: err});
+
+			var configuraciones;
+
+			//si no existe una configuraciones se inicializa.
+			if(configuracionesLst.length == 0){
+				var configuraciones = crearConfiguraciones(req.body);
+				mensaje = "configuraciones creada";
+			} else {
+				//si existe tomar la primera
+				configuraciones = configuracionesLst[0];
+				mensaje = "configuraciones actualizada"
+			}
+
+			configuraciones.save(function(err){
+				res.json({
+					success: true,
+					configuraciones: configuraciones,
+					mensaje: mensaje
+				});
+			});
+		});
+	});
+
+	router.post('/sobre-empresa',	function(req, res){
 		Configuraciones.find(function(err, configuracionesLst) {
 			if (err) return res.json({success: false, mensaje: err.errmsg, error: err});
 
@@ -35,14 +87,39 @@ module.exports = function(app, passport) {
 
 			//si no existe una configuraciones se inicializa.
 			if(configuracionesLst.length == 0){
-				configuraciones = Configuraciones({
-					domicilio: req.body.domicilio
-				});
-				mensaje = "configuraciones creada"
+				var configuraciones = crearConfiguraciones(req.body);
+				mensaje = "configuraciones creada";
 			} else {
 				//si existe tomar la primera
 				configuraciones = configuracionesLst[0];
-				configuraciones.domicilio = req.body.domicilio;
+				mensaje = "configuraciones actualizada"
+			}
+			configuraciones.save(function(err){
+				res.json({
+					success: true,
+					configuraciones: configuraciones,
+					mensaje: mensaje
+				});
+			});
+
+			
+		});
+	});
+
+	router.post('/terminos-condiciones',	function(req, res){
+		Configuraciones.find(function(err, configuracionesLst) {
+			if (err) return res.json({success: false, mensaje: err.errmsg, error: err});
+
+			var configuraciones = null;
+
+			//si no existe una configuraciones se inicializa.
+			if(configuracionesLst.length == 0){
+				var configuraciones = crearConfiguraciones(req.body);
+				mensaje = "configuraciones creada";
+			} else {
+				//si existe tomar la primera
+				configuraciones = configuracionesLst[0];
+				configuraciones.terminosCondiciones = req.body.terminosCondiciones;
 				mensaje = "configuraciones actualizada"
 			}
 			configuraciones.save(function(err){
