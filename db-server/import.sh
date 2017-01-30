f908c54594d45f794feff2c4cf2f859c732a3882
wfpc92 <wfpc92@hotmail.com>
2017-01-30 17:38:44 -0500

    export database mongo json format

---
+++ db-server/import.sh
@@ -0,0 +1,12 @@
+mongoimport -d Cleansuit -c clientes  clientes.json
+mongoimport -d Cleansuit -c configuraciones  configuraciones.json
+mongoimport -d Cleansuit -c identitycounters  identitycounters.json
+mongoimport -d Cleansuit -c ordenes  ordenes.json
+mongoimport -d Cleansuit -c productos  productos.json
+mongoimport -d Cleansuit -c promociones  promociones.json
+mongoimport -d Cleansuit -c servicios  servicios.json
+mongoimport -d Cleansuit -c subservicios subservicios.json
+mongoimport -d Cleansuit -c usuarios  usuarios.json
+mongoimport -d Cleansuit -c versionapps  versionapps.json
+mongoimport -d Cleansuit -c versionesordenes versionesordenes.json
+
