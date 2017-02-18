cd2a565ae6dd50994f968e2c76c9a4bc050d555e
Wilian Fernando Pantoja Carvajal <wfpc92@hotmail.com>
2017-02-18 04:59:18 -0500

    consulta de prenda con codigo, se envia la informaicon individual de la prenda, con informacion de subservicio y orden

--- routes/prendas.js
+++ routes/prendas.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var Prendas = require('../models/prendas');
 var Ordenes = require('../models/ordenes');
+var Subservicios = require('../models/subservicios');
 
 module.exports = function(app, passport) {
 	router.use(passport.authenticate('jwt', { session: false}));
@@ -9,30 +10,52 @@ module.exports = function(app, passport) {
 	router.get('/codigo/:codigo', function(req, res) {
 		var codigo = req.params.codigo;
 
-		Ordenes.find(function(err, ordenes) {
+		Ordenes
+		.find(function(err, ordenes) {
 			if (err) return res.json({success: false, mensaje: err.errmsg, error: err});
+			var encontroPrenda = false;
 
 			for (var i in ordenes) {
 				if(ordenes[i].recoleccion) {
 					for (var j in ordenes[i].recoleccion.items.prendas) {
-						if (ordenes[i].recoleccion.items.prendas[j].codigo = codigo) {
+						if (ordenes[i].recoleccion.items.prendas[j].codigo == codigo) {
 							prenda = ordenes[i].recoleccion.items.prendas[j];
-							prenda.orden_id = ordenes[i]._id;
-							return res.json({
-								success: true,
-								prenda: prenda,
-								mensaje: 'prenda de codigo '+codigo
-							});
+							console.log(ordenes[i])
+							prenda.orden = {};
+							prenda.orden.codigo =  ordenes[i].codigo;
+							encontroPrenda = true;
+							break;
 						}
+					}
+					if(encontroPrenda) {
+						break;
 					}
 				}
 			}
 
-			res.json({
-				success: true,
-				mensaje: 'no se encuentra informacion de prenda de codigo '+codigo
-			});
-			
+			if (encontroPrenda) {
+				Subservicios
+				.findOne({_id: prenda.subservicio._id})
+				.populate('_creator')
+				.exec(function(err, subservicios) {
+					if (err) return res.json({success: false, mensaje: err.errmsg, error: err});
+					
+					prenda.subservicio = subservicios;
+					delete prenda.servicio;
+					
+					res.json({
+						success: true,
+						prenda: prenda,
+						mensaje: 'prenda de codigo '+codigo
+					});
+					
+				});	
+			} else {
+				res.json({
+					success: true,
+					mensaje: 'no se encuentra informacion de prenda de codigo '+codigo
+				});
+			}			
 		});
 	});
 	
