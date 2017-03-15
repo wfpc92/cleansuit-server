e001efa75d71a180df6d4bb4ee7df72f1debc289
Mateo TibaPalacios <nestormateo@gmail.com>
2017-03-15 10:40:41 -0500

    ref(db): fb_uid renamed to facebook and reworked scripts

--- db-server/export.sh
+++ db-server/export.sh
@@ -1,12 +1,4 @@
-mongoexport -d Cleansuit -c clientes -o clientes.json
-mongoexport -d Cleansuit -c configuraciones -o configuraciones.json
-mongoexport -d Cleansuit -c identitycounters -o identitycounters.json
-mongoexport -d Cleansuit -c ordenes -o ordenes.json
-mongoexport -d Cleansuit -c productos -o productos.json
-mongoexport -d Cleansuit -c promociones -o promociones.json
-mongoexport -d Cleansuit -c servicios -o servicios.json
-mongoexport -d Cleansuit -c subservicios -o subservicios.json
-mongoexport -d Cleansuit -c usuarios -o usuarios.json
-mongoexport -d Cleansuit -c versionapps -o versionapps.json
-mongoexport -d Cleansuit -c versionesordenes -o versionesordenes.json
-
+#!/bin/bash
+for i in *.json; do
+    mongoexport -d Cleansuit -c ${i/.json/} -o $i
+done
--- db-server/import.sh
+++ db-server/import.sh
@@ -1,12 +1,4 @@
-mongoimport -d Cleansuit -c clientes  clientes.json
-mongoimport -d Cleansuit -c configuraciones  configuraciones.json
-mongoimport -d Cleansuit -c identitycounters  identitycounters.json
-mongoimport -d Cleansuit -c ordenes  ordenes.json
-mongoimport -d Cleansuit -c productos  productos.json
-mongoimport -d Cleansuit -c promociones  promociones.json
-mongoimport -d Cleansuit -c servicios  servicios.json
-mongoimport -d Cleansuit -c subservicios subservicios.json
-mongoimport -d Cleansuit -c usuarios  usuarios.json
-mongoimport -d Cleansuit -c versionapps  versionapps.json
-mongoimport -d Cleansuit -c versionesordenes versionesordenes.json
-
+#!/bin/bash
+for i in *.json; do
+    mongoimport -d Cleansuit -c ${i/.json/} --file $i
+done
