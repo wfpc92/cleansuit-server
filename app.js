bec332f6e133d599d6bea0e569ca4a352bccc559
Mateo TibaPalacios <nestormateo@gmail.com>
2017-03-17 11:17:28 -0500

    add(compression): gzip optimization

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
 var express 		= require('express');
+var compression     = require('compression');
 var path           	= require('path');
 var favicon 		= require('serve-favicon');
 var logger 			= require('morgan');
@@ -29,6 +30,7 @@ function runapp() {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(cookieParser());
+    app.use(compression());
     app.use(express.static(path.join(__dirname, 'public')));
 
     // required for passport
